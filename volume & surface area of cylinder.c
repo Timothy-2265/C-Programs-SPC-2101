8cd4db6073a97f1df52050e5ab1dd50570868dd1
Timothy-2265 <timothykahuki60@gmail.com>
2025-09-22 08:49:09 +0300

    Add files via upload

---
+++ volume & surface area of cylinder.c
@@ -0,0 +1,42 @@
+/*Name:Timothy Kahuki
+Reg no:CT101/G/26459/25
+Date:22/9/2025
+Description:First year computer science units
+volume=PI*r^2*h
+surface area=(2*PI*r^2)+(2*PI*r*h)
+*/
+#include<stdio.h>
+int main()
+{
+	//variable decraration
+	float PI=3.142;
+	float Radius;
+	float height;
+	float surface_area;
+	float volume;
+	
+	//prompt user to enter height and radius of the cylinder
+	printf("Enter Radius of the cylinder\n");
+	
+	scanf("%f",&Radius);
+	
+	printf("Enter height of the cylinder\n");
+	
+	scanf("%f",&height);
+	
+	//formula for volume and surface area calculation for cylinder
+	volume=PI*Radius*Radius*height;
+surface_area=(2*PI*Radius*Radius)+(2*PI*Radius*height);
+
+//print the output to user screen
+printf("volume of the cylinder=%.2f",volume);
+printf("\nsurface_area of cylinder=%.2f",surface_area);
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}
