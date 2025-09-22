389ac8d5bf22d9cda2618b5bc08c76476d782419
Timothy-2265 <timothykahuki60@gmail.com>
2025-09-22 08:40:53 +0300

    Add files via upload

---
+++ 1.1 Units.c
@@ -0,0 +1,30 @@
+/*Name:Timothy Kahuki
+Reg no:CT101/G/26459/25
+Date:12/9/2025
+Description:First year computer science units
+*/
+
+#include<stdio.h>
+int main()
+{
+	
+printf("Unit:1 Fundamentals of ICT(ucu2105)\n");
+
+printf("Unit:2 Introduction to computer architecture(spc2102)\n");
+
+printf("Unit:3 Analogue electronics(sph2107)\n");
+
+printf("Unit:4 Discrete mathematics(spm2100)\n");
+
+printf("Unit:5 Mathematics for science(spm2104)\n");
+
+printf("Unit:6 Physics(sph2101)\n");
+
+printf("Unit:7 Introduction to programming and problem solving(spc2101)\n");
+
+printf("Unit:8 Communication skills(ucu2101)\n");
+
+
+	
+	return 0;
+}
