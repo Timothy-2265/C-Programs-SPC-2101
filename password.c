349e1cb92dfa4ebe8720c384b182423a0816939f
Timothy-2265 <timothykahuki60@gmail.com>
2025-10-09 14:07:43 +0300

    Update and rename passwaord.c to password.c

--- password.c
+++ password.c
@@ -6,16 +6,19 @@ Description:for program to enter the password
 #include <stdio.h>
 int main(int argc, char** argv)
 {
-	
+
+	//variables decraration
 	int password;
-	int correct_password=1234;
+	int correct_password=1234;//start
 	
 	do{
+		// prompt user to enter the password
 		printf("Enter the password:  ");
 		scanf("%d",&password);
 		
-	}while(password != correct_password);
+	}while(password != correct_password);//stop
 	
-	printf("Access Guranted\n");
+	printf("Access Guranted\n");//display when correct password is entered
 	return 0;
+
 }
