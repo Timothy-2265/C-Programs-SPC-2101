b8614be5a944e1ec0460a7117e0022acb162acb9
Timothy-2265 <timothykahuki60@gmail.com>
2025-10-09 11:27:10 +0300

    Add files via upload

---
+++ numbers_100-50.c
@@ -0,0 +1,22 @@
+/*Name:Timothy Kahuki
+Reg no:CT101/G/26459/25
+Date:4/10/2025
+Description:for program to enter values 100-50
+*/
+#include <stdio.h>
+int main()
+{
+	//variable decraration
+	float z=0;
+	
+	//start,stop,step
+	for(z=100;z>=50;z-=1)
+	{
+		
+    //print the numbers
+	printf("%.0f\n",z);	
+	}
+	
+	//execution successful
+	return 0;
+}
