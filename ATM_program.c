a4076fa3613cbb453c2ec58d6ba0586f04fe0615
Timothy-2265 <timothykahuki60@gmail.com>
2025-10-09 11:58:45 +0300

    Update ATM_program.c

--- ATM_program.c
+++ ATM_program.c
@@ -6,25 +6,28 @@ Description:ATM program to withdraw amount
 #include <stdio.h>
 int main()
 {
-	int balance=6000;
+	// variable decraration
+	int balance=6000;//start
 	int amount;
 	
 	printf("..............................\n");
 	
 	printf("DO NOT SHARE YOUR PIN\n");
-	printf("\nThe balance is 6000\n");
+	printf("\nThe balance is 6000\n");// print initial balance
 	
-	while(balance>0)
+	while(balance>0)//stop
 	{
+		//prompt user enter amount to withdraw
 		printf("Enter amount to withdraw\n");
 		scanf("%d",&amount);
 		
-		balance -= amount	;
+		balance -= amount	;//deduction per withdrawal
 		
-		printf("The balance is %d\n",balance);
+		printf("The balance is %d\n",balance);//balance after withdrawal
 	}
 	printf("insufficient balance\n");
 	
 	
 	return 0;
+
 }
