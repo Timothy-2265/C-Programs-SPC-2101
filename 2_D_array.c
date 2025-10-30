9594368724f0c08673c23f948c4c5e0c0b58e779
Timothy-2265 <timothykahuki60@gmail.com>
2025-10-30 21:39:26 +0300

    Add files via upload

---
+++ 2_D_array.c
@@ -0,0 +1,33 @@
+/*Name:Timothy Kahuki
+Reg no:CT101/G/26459/25
+Date:30/10/2025
+Description:Two D array
+*/
+#include <stdio.h>
+
+int main() {
+    int occupancy[5][10];
+    int occupied, vacant;
+
+    printf("=== Room Occupancy (One Branch) ===\n");
+    printf("(Automatically assigning occupancy values...)\n");
+
+    for (int floor = 0; floor < 5; floor++) {
+        occupied = vacant = 0;
+        for (int room = 0; room < 10; room++) {
+            
+            occupancy[floor][room] = (floor + room) % 2;
+
+            if (occupancy[floor][room] == 1)
+                occupied++;
+            else
+                vacant++;
+        }
+
+        printf("Floor %d -> Occupied: %d, Vacant: %d\n", floor + 1, occupied, vacant);
+    }
+
+    return 0;
+}
+
+         
